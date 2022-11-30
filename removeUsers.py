97f9c6f3417a24e533fcabb01b8e81ba121be38e
calebkpai <119488597+calebkpai@users.noreply.github.com>
2022-11-30 01:33:03 -0800

    Add files via upload

---
+++ removeUsers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import subprocess
+
+expectedUsers = ["kali","bigbee","smolbee","swolbee","jimbee","derbee","gabee"] # briefing packet users
+users = [] # empty array to hold user names from box
+
+subprocess.run(["touch","userList.txt"]) # creates textfile "userList.txt"
+subprocess.run(["awk","F:","'$3",">=","1000","&&","$1","!=","nobody","{print $1}'","/etc/passwd"]) # outputs users on machine and copies output into "userList.txt"
+
+
+with open("userList.txt") as file: # inputs each name line by line into the user array
+    users = [line.rstrip() for line in file]
+
+for test in users: # if the box has users that are not from expectedUsers, it will delete them
+    counter = 7
+    for expected in expectedUsers:
+            if(test == expected):
+                counter = counter - 1
+    if (counter == 7):
+        subprocess.run(["sudo","userdel","-r",test])
+
